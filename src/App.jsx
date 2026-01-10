fdc155a220387fada1b636429434bf8f5ebea780
Saad Sohail <saadsohail82733@gmail.com>
2026-01-10 05:05:12 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { useDispatch } from 'react-redux'
-import authService from './appwrite/auth'
+import authService from './appwrite/auth.js'
 import {login, logout} from './store/authSlice'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
