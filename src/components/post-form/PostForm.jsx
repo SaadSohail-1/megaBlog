f7b2c781ac37fa3082fd5d59fa5364f869c14bde
Saad Sohail <saadsohail82733@gmail.com>
2026-01-10 05:22:25 -0600

    rename config.js to posts_config.js to fix build

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,7 +1,7 @@
 import React, {useCallback, useEffect} from 'react'
 import { useForm } from 'react-hook-form'
 import {Button, Input, Select, RTE} from '../index'
-import appwriteService from '../../appwrite/config'
+import appwriteService from '../../appwrite/posts_config'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 // import RTE from '../index'
