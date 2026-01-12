4a66df9f97ed2538176d0035240998908d5ca919
Saad Sohail <saadsohail82733@gmail.com>
2026-01-12 11:36:30 -0600

    fixed env vars error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,7 @@ function Home() {
             <Container>
                 <div className='flex flex-wrap'>
                     <div className="p-2 w-full">
-                        <h1 className='text-2xl font-bold hover:text-gray-500'>
+                        <h1 className='text-2xl font-bold text-gray-400 hover:text-white'>
                             Login to read posts
                         </h1>
                     </div>
